971e2f6d87eae0211eac1c5e79a8347f090e78bd
avitaind <52917489+avitaind@users.noreply.github.com>
2020-07-08 13:14:00 +0530

    Update feature_hk_en.blade.php

--- resources/views/product/pura/feature_hk_en.blade.php
+++ resources/views/product/pura/feature_hk_en.blade.php
@@ -142,8 +142,8 @@
 			</div>
 			  <div class="data-card my-3 text-left">
                                 <div class="badge-caption">Up to</div>
-				<div class="badge-value">Intel&reg; | AMD</div>
-                                <div class="badge-caption">CORE® i3-8145U / i5-8265U | AMD A9-9420E / R3-3200U / R5-3500U </div>
+				<div class="badge-value">Intel<sup>&reg;</sup> | AMD</div>
+                                <div class="badge-caption">CORE™ i3-8145U / i5-8265U | AMD A9-9420E / R3-3200U / R5-3500U </div>
 			</div>
 		   </div>	
                 </div>
