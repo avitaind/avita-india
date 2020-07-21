6340707f58aabbeceb79d5fc3de78e1d98eee52d
AVITA India <avitaind@gmail.com>
2020-07-21 14:47:28 +0530

    uncaught error js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -145,6 +145,7 @@
     @yield('css')
    
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" async></script> 
+    <script async type="text/javascript" src="{{ asset('js/demo.js') }}"></script>
 
 <script type="text/javascript">
 window.addEventListener('load', 
@@ -221,7 +222,6 @@ window.addEventListener('load',
 
 <script async type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
 <script async type="text/javascript" src="{{ asset('js/conversion.js') }}"></script>
-<script async type="text/javascript" src="{{ asset('js/demo.js') }}"></script>
 
 <script async type="text/javascript" src="{{asset('/js/wpk4iql.js')}}"></script>
 <script async type="text/javascript" src="{{asset('/js/support.js')}}"></script>
