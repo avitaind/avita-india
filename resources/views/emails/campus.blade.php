17424b2295ac64a3021757940a1a91eefb9b17af
AVITA India <avitaind@gmail.com>
2020-08-13 15:55:28 +0530

    cap page changes

--- resources/views/emails/campus.blade.php
+++ resources/views/emails/campus.blade.php
@@ -1,4 +1,2 @@
 <h2>Dear {{ $name }},</h2>
 <img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" />
-
-<a href="{{  $message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf') }}" target="_blank" download="{{  $message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf') }}">Download File</a>
--- storage/framework/views/4568710173018d7126bfa8b8a5edbb841062e71c.php
+++ storage/framework/views/4568710173018d7126bfa8b8a5edbb841062e71c.php
@@ -1,3 +1,4 @@
 <h2>Dear <?php echo e($name); ?>,</h2>
 <img src="<?php echo e($message->embed(public_path() . '/images/cap-auto-reply.png')); ?>" alt="" />
 
+<!--- <a href="<?php echo e($message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf')); ?>" target="_blank" download="<?php echo e($message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf')); ?>">Download File</a> --->
