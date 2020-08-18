204c153494cd8ac46f0bbd0cb7e7314a0f2fbd68
AVITA India <avitaind@gmail.com>
2020-08-18 18:49:02 +0530

    api issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,10 +65,9 @@
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css"> -->
     <!--<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWQCKIl7B4w27KcSW-tW4ja_Rk4SbcPnk&callback=initMap" type="text/javascript"></script>-->
    <!--- <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script> -->
-   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
-
 
     <script>try{Typekit.load();}catch(e){}</script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
