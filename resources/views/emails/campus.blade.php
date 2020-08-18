740724556b69be9fa6ba6f0db94a83269195afed
AVITA India <avitaind@gmail.com>
2020-08-18 17:41:36 +0530

    api issue

--- resources/views/emails/campus.blade.php
+++ resources/views/emails/campus.blade.php
@@ -1,2 +1,5 @@
 <h2>Dear {{ $name }},</h2>
-<img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" />
+<img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" /><br/>
+<span>
+<a href="{{ $message->embed(public_path().'/images/AVITA-Family-Brochure.pdf') }}" target="_blank" download="{{ public_path().'/images/AVITA-Family-Brochure.pdf' }}">Download File</a>
+</span>
--- storage/framework/views/4568710173018d7126bfa8b8a5edbb841062e71c.php
+++ storage/framework/views/4568710173018d7126bfa8b8a5edbb841062e71c.php
@@ -1,4 +1,5 @@
 <h2>Dear <?php echo e($name); ?>,</h2>
 <img src="<?php echo e($message->embed(public_path() . '/images/cap-auto-reply.png')); ?>" alt="" />
-
-<!--- <a href="<?php echo e($message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf')); ?>" target="_blank" download="<?php echo e($message->attach(public_path() . '/images/AVITA-Family-Brochure.pdf')); ?>">Download File</a> --->
+<span class="btn btn-info">
+<a href="<?php echo e($message->embed(public_path().'/images/AVITA-Family-Brochure.pdf')); ?>" target="_blank" download="<?php echo e(public_path().'/images/AVITA-Family-Brochure.pdf'); ?>">Download File</a>
+</span>
