f8e266777b83275b0b815b7a10c015d43c7b3416
AVITA India <avitaind@gmail.com>
2020-08-19 20:02:58 +0530

    campus mail format v7

--- resources/views/emails/campus.blade.php
+++ resources/views/emails/campus.blade.php
@@ -1,4 +1,6 @@
 <h2>Dear {{ $name }},</h2>
+<p>Please find attached to this mail a pdf file for 'Declaration by the candidate'. You must sign this document and attach it to the mail if you wish to enroll in the program.</p>
+
 <img src="{{ $message->embed(public_path() . '/images/cap-auto-reply.png') }}" alt="" />
 
 <span class="btn btn-primary">
