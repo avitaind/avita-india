4914aa722e9925b6ce28d8bada73e248fc3236fc
AVITA India <avitaind@gmail.com>
2020-10-20 16:14:14 +0530

    dropdown enabled

--- resources/views/partials/liber-v-gold-navbar.blade.php
+++ resources/views/partials/liber-v-gold-navbar.blade.php
@@ -4,13 +4,22 @@
             <i class="product-nav-icon fa fa-angle-up" aria-hidden="true"></i>
         </div>
 
-         <div class="product-nav-collapse">
-            <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
-            <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.overview', ['liber-v-gold']) }}">@lang('site.productnav_overview_liber_v_gold')</a></li>
+        <div class="product-nav-collapse">
+        <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
+        <div class="dropdown">
+         <button class="dropbtn"><a class="nav-link py-1" href="{{ route('product.overview', ['liber-v-gold']) }}">@lang('site.productnav_overview_liber_v_gold')</a></button>
+                <div class="dropdown-content">
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber-v-gold-black']) }}">@lang('site.liber-v-gold-black')</a></li>
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber-v-gold-blue']) }}">@lang('site.liber-v-gold-blue')</a></li>
+    
+              </div>
+              
+            </div>
             <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.spec', ['liber-v-gold']) }}">@lang('site.productnav_spec')</a></li>
-            <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.support', ['liber']) }}">@lang('site.productnav_support')</a></li>
-            <li class="nav-item"><a class="nav-link py-1" href="/where-to-buy">@lang('site.productnav_wheretobuy')</a></li>
-           </ul>
+              <li class="nav-item"><a class="nav-link py-1" href="{{ route('product.support', ['liber']) }}">@lang('site.productnav_support')</a></li>
+              <li class="nav-item"><a class="nav-link py-1" href="/where-to-buy">@lang('site.productnav_wheretobuy')</a></li>
+             
+            </ul>
         </div>
     </div>
 </div>
--- storage/framework/views/cb24f7b98a6cf14c8ebe0c7a7b0a427ad1331533.php
+++ storage/framework/views/cb24f7b98a6cf14c8ebe0c7a7b0a427ad1331533.php
@@ -4,35 +4,22 @@
             <i class="product-nav-icon fa fa-angle-up" aria-hidden="true"></i>
         </div>
 
-         <div class="product-nav-collapse">
-            <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
-                    
-          <div class="dropdown">
-           <ul class="list-unstyled">
-            <li class="nav-item has-dropdown">
-               <input id="header_product_liber" type="checkbox" hidden="">
-                     <a class="nav-link py-1" href="<?php echo e(route('product.overview', ['liber-v-gold'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_overview_liber_v_gold'); ?></a>
-                                <div class="dropdown">
-                                    <ul class="list-unstyled">
-                                    <li class="nav-item">
-                                          <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-black'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-black'); ?></a>
-                                       </li>
-                                    
-                                       <li class="nav-item">
-                                          <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-blue'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-blue'); ?></a>
-                                       </li>
-
-                                       </ul>
-                                </div>
-                            </li>
-                     </ul>
-                 </div>
-            </li>
-                
+        <div class="product-nav-collapse">
+        <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
+        <div class="dropdown">
+         <button class="dropbtn"><a class="nav-link py-1" href="<?php echo e(route('product.overview', ['liber-v-gold'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_overview_liber_v_gold'); ?></a></button>
+                <div class="dropdown-content">
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-black'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-black'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-blue'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-blue'); ?></a></li>
+    
+              </div>
+              
+            </div>
             <li class="nav-item"><a class="nav-link py-1" href="<?php echo e(route('product.spec', ['liber-v-gold'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_spec'); ?></a></li>
-            <li class="nav-item"><a class="nav-link py-1" href="<?php echo e(route('product.support', ['liber'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_support'); ?></a></li>
-            <li class="nav-item"><a class="nav-link py-1" href="/where-to-buy"><?php echo app('translator')->getFromJson('site.productnav_wheretobuy'); ?></a></li>
-           </ul>
+              <li class="nav-item"><a class="nav-link py-1" href="<?php echo e(route('product.support', ['liber'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_support'); ?></a></li>
+              <li class="nav-item"><a class="nav-link py-1" href="/where-to-buy"><?php echo app('translator')->getFromJson('site.productnav_wheretobuy'); ?></a></li>
+             
+            </ul>
         </div>
     </div>
 </div>
