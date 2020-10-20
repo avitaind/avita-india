eec774a51f9c92522e077683e8ad8a2b3b1c5076
AVITA India <avitaind@gmail.com>
2020-10-20 21:01:16 +0530

    dropdown button changes

--- resources/views/partials/liber-v-gold-navbar.blade.php
+++ resources/views/partials/liber-v-gold-navbar.blade.php
@@ -7,7 +7,7 @@
         <div class="product-nav-collapse">
         <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
         <div class="dropdown">
-         <button class="dropbtn"><a class="nav-link py-1" href="{{ route('product.overview', ['liber-v-gold']) }}">@lang('site.productnav_overview_liber_v_gold')</a></button>
+         <span class="dropbtn"><a class="nav-link py-1" href="{{ route('product.overview', ['liber-v-gold']) }}">@lang('site.productnav_overview_liber_v_gold')</a></span>
                 <div class="dropdown-content">
                     <li class="nav-item"><a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber-v-gold-black']) }}"><small>@lang('site.liber-v-gold-black')</small></a></li>
                     <li class="nav-item"><a class="nav-link px-md-4 py-2" href="{{ route('product.overview', ['liber-v-gold-blue']) }}"><small>@lang('site.liber-v-gold-blue')</small></a></li>
