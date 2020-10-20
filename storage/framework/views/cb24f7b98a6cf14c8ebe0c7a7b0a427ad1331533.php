b5f702ca25062cc2b8bb38272c30b3685c1fa64f
AVITA India <avitaind@gmail.com>
2020-10-20 19:16:57 +0530

    hide + sign form dorpdown

---
+++ storage/framework/views/cb24f7b98a6cf14c8ebe0c7a7b0a427ad1331533.php
@@ -0,0 +1,25 @@
+<div class="nav-product-panel">
+    <div class="container">
+        <div class="product-nav-toggler h2 text-center my-1 hidden-sm-up">
+            <i class="product-nav-icon fa fa-angle-up" aria-hidden="true"></i>
+        </div>
+
+        <div class="product-nav-collapse">
+        <ul class="nav-product d-flex list-unstyled flex-column justify-content-center text-center flex-sm-row justify-content-sm-end my-2">
+        <div class="dropdown">
+         <button class="dropbtn"><a class="nav-link py-1" href="<?php echo e(route('product.overview', ['liber-v-gold'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_overview_liber_v_gold'); ?></a></button>
+                <div class="dropdown-content">
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-black'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-black'); ?></a></li>
+                    <li class="nav-item"><a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-blue'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold-blue'); ?></a></li>
+    
+              </div>
+              
+            </div>
+            <li class="nav-item"><a class="nav-link py-1" href="<?php echo e(route('product.spec', ['liber-v-gold'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_spec'); ?></a></li>
+              <li class="nav-item"><a class="nav-link py-1" href="<?php echo e(route('product.support', ['liber'])); ?>"><?php echo app('translator')->getFromJson('site.productnav_support'); ?></a></li>
+              <li class="nav-item"><a class="nav-link py-1" href="/where-to-buy"><?php echo app('translator')->getFromJson('site.productnav_wheretobuy'); ?></a></li>
+             
+            </ul>
+        </div>
+    </div>
+</div>
