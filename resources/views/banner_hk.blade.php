18dbd463df92b21fdca61632c0d74172a9d68270
avitaind <52917489+avitaind@users.noreply.github.com>
2020-11-17 19:36:10 +0530

    Update banner_hk.blade.php

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -5,75 +5,38 @@
 
         <div class="carousel-item active">
                 <div class="responsive-block">
-                <a href="{{ route('product.overview', ['liber-v-gold-blue']) }}">
                     <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-gold"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-gold"></div>
+                        <div class="banner-bg hidden-sm-down banner-web-essential"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-essential"></div>
                     </div>
-                    </a>
                 </div>
             </div>
             
-       <!---
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="https://gifts.avita-india.com/">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-goa"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-goa"></div>
-                    </div>
-                    </a>
-                </div>
-            </div>
-
-    ---->
-        <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="https://in.nexstmall.com/">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-f1"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-f1"></div>
-                    </div>
-                    </a>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="/where-to-buy">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-bfl1"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-bfl1"></div>
-                    </div>
-                    </a>
-                </div>
-            </div>
-
-       
-
-
-            
-        <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="https://in.nexstmall.com/">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-f2"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-f2"></div>
-                    </div>
-                    </a>
-                </div>
-            </div>
-
     
-     
+        <div class="carousel-item">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item">
+                        <div class="banner-bg hidden-sm-down banner-web-admiror"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-admiror"></div>
+                    </div>
+                </div>
+            </div>
+
             <div class="carousel-item">
                 <div class="responsive-block">
-                <a href="/where-to-buy">
                     <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-bfl2"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-bfl2"></div>
+                        <div class="banner-bg hidden-sm-down banner-web-pura"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-pura"></div>
                     </div>
-                    </a>
+                </div>
+            </div>
+
+        <div class="carousel-item">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item">
+                        <div class="banner-bg hidden-sm-down banner-web-magus"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-magus"></div>
+                    </div>
                 </div>
             </div>
 
