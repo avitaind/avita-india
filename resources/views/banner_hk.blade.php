a1fe38667d4aa077d6681509b1aa4ec268228eb8
avitaind <52917489+avitaind@users.noreply.github.com>
2020-11-18 15:03:38 +0530

    Update banner_hk.blade.php

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -13,6 +13,15 @@
             </div>
             
   
+    <div class="carousel-item">
+                <div class="responsive-block">
+                    <div class="banner-block responsive-item">
+                        <div class="banner-bg hidden-sm-down banner-web-liber-v"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-liber-v"></div>
+                    </div>
+                </div>
+            </div>
+
         <div class="carousel-item">
                 <div class="responsive-block">
                     <div class="banner-block responsive-item">
@@ -55,6 +64,8 @@
            <li data-target="#home_banner" data-slide-to="1"></li>
            <li data-target="#home_banner" data-slide-to="2"></li>
            <li data-target="#home_banner" data-slide-to="3"></li>
+           <li data-target="#home_banner" data-slide-to="4"></li>
+           <li data-target="#home_banner" data-slide-to="5"></li>
 
         </ol>
     </div>
