a2a09792ce278a78c7efd30c88ae92ec6ebac2f9
avitaind <avitaind@gmail.com>
2021-02-24 16:21:16 +0530

    contact us page

--- storage/framework/views/0825079c102d40fdb95e2900de964e340bb6f5fa.php
+++ storage/framework/views/0825079c102d40fdb95e2900de964e340bb6f5fa.php
@@ -1,16 +1,14 @@
-@extends('layouts.app')
+<?php $__env->startSection('title'); ?>
+    <?php echo app('translator')->getFromJson('title.contact_us'); ?>
+<?php $__env->stopSection(); ?>
 
-@section('title')
-    @lang('title.contact_us')
-@stop
-
-@section('content')
+<?php $__env->startSection('content'); ?>
 
     <main>
         <section class="aboutus-panel top-nav-padding ls-0">
             <div class="aboutus-contact py-5 px-2">
                 <div class="container">
-                    <h1 class="section-title mt-0 mb-5 my-md-5 font-weight-light">@lang('site.contactus_contactus') </h1>
+                    <h1 class="section-title mt-0 mb-5 my-md-5 font-weight-light"><?php echo app('translator')->getFromJson('site.contactus_contactus'); ?> </h1>
 
                     <div class="row">
                         <div class="col-sm-6">
@@ -22,7 +20,7 @@
                                 <div class="mb-2"><b>Email:</b><br>
                                     Insales@nexstgo.com</div>
 
-                                <div><b>@lang('site.contactus_operating')</b><br>
+                                <div><b><?php echo app('translator')->getFromJson('site.contactus_operating'); ?></b><br>
                                     Monday to Friday: 9am - 9pm</div>
                             </div>
                      
@@ -54,4 +52,6 @@
 
     </main>
 
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
