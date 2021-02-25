3b5289fd6e5d7019b03d27bc2d4a8590df9d404f
avitaind <avitaind@gmail.com>
2021-02-25 17:39:25 +0530

    avita india support dropdown

--- resources/views/pages/support.blade.php
+++ resources/views/pages/support.blade.php
@@ -215,9 +215,8 @@ Due to the global outbreak of the coronavirus (COVID-19), the service center wil
 
 
 
-    <script type="text/javascript" src="{{ asset('js/support.js') }}"></script>
+    <script type="text/javascript" defer src="{{ asset('js/support.js') }}"></script>
 
-    <script type="text/javascript" src="{{ asset('js/demo.js') }}"></script>
 
 
 @endsection
