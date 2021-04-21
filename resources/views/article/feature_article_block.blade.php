beb3876cdaf2367ef21e1ec4fa790983efbf3496
GOD OF THUNDER <avitaind@gmail.com>
2021-04-21 11:42:18 +0530

    news article red dot

--- resources/views/article/feature_article_block.blade.php
+++ resources/views/article/feature_article_block.blade.php
@@ -5,7 +5,7 @@
                 <img style="width: 100%;" src="{{ $article->thumbnail_url() }}" />
             </div>
             <figcaption class="d-flex event-caption {{ $article->status }} px-4 py-1">
-                <div class="event-date"><i class="fa fa-calendar-o mr-3" aria-hidden="true"></i>{{ $article->eventDateString() }}</div>
+                <div class="event-date"><i class="fa fa-calendar-o mr-3" aria-hidden="true"></i>{{ $article->start_date->toDateString() }}</div>
                 <div class="event-publication {{ $article->status }} ml-auto">@lang('site.news_status_'.$article->status)</div>
             </figcaption>
         </figure>
--- storage/framework/views/43c34c46ede64c3ed143a3bfd843d01ab120a773.php
+++ storage/framework/views/43c34c46ede64c3ed143a3bfd843d01ab120a773.php
@@ -5,7 +5,7 @@
                 <img style="width: 100%;" src="<?php echo e($article->thumbnail_url()); ?>" />
             </div>
             <figcaption class="d-flex event-caption <?php echo e($article->status); ?> px-4 py-1">
-                <div class="event-date"><i class="fa fa-calendar-o mr-3" aria-hidden="true"></i><?php echo e($article->eventDateString()); ?></div>
+                <div class="event-date"><i class="fa fa-calendar-o mr-3" aria-hidden="true"></i><?php echo e($article->start_date->toDateString()); ?></div>
                 <div class="event-publication <?php echo e($article->status); ?> ml-auto"><?php echo app('translator')->getFromJson('site.news_status_'.$article->status); ?></div>
             </figcaption>
         </figure>
