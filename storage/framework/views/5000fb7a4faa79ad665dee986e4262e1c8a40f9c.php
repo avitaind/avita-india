295971379233b5f54a73df30a96c42208dd7f3c8
avitaind <avitaind@gmail.com>
2021-05-10 22:19:39 +0530

    liber v

--- storage/framework/views/5000fb7a4faa79ad665dee986e4262e1c8a40f9c.php
+++ storage/framework/views/5000fb7a4faa79ad665dee986e4262e1c8a40f9c.php
@@ -1,25 +1,23 @@
-@extends('layouts.app')
+<?php $__env->startSection('title'); ?>
+    <?php echo app('translator')->getFromJson('title.LIBER_v_home'); ?>
+<?php $__env->stopSection(); ?>
 
-@section('title')
-    @lang('title.LIBER_v_home')
-@stop
-
-@section('content')
+<?php $__env->startSection('content'); ?>
     <main class="top-nav-padding">
 
-        @include('partials.liber-v-navbar')
-		@include('partials.rolling-cta')
+        <?php echo $__env->make('partials.liber-v-navbar', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+		<?php echo $__env->make('partials.rolling-cta', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
 
         <section class="product-liber-banner">
             <div class="responsive-block">
                 <div class="banner-block responsive-item">
                     <div class="banner-bg hidden-sm-down" style="background-image: url('/images/banner/avita-liber-v-web-new-v1.jpg')"></div>
                     <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/avita-liber-v-mob-new-v1.jpg')"></div>
-                    {{-- <div class="banner-bg hidden-sm-down" style="background-image: url('/images/banner/Avita_Liber_V_Red_Dot_Award_Web.jpg')"></div> --}}
-                    {{-- <div class="banner-bg hidden-md-up" style="background-image: url('/images/banner/Avita_Liber_V_Red_Dot_Award_Mob.jpg')"></div> --}}
+                    
+                    
                     <div class="banner-info">
                         <div class="btn-group mt-3">
-                            {{--<img class="mx-auto" src="/images/liber-v/brand.png">--}}
+                            
                         </div>
                         <div class="an-scroll-wrap">
                             <div class="an-scroll">
@@ -72,8 +70,8 @@
             <div class="responsive-block"> 
                 <div class="banner-bg hidden-sm-down" style="background-image: url('/images/liber-v/Avita_Liber_V_Red_Dot_Award_Web.jpg')"></div>
                 <div class="banner-bg hidden-md-up" style="background-image: url('/images/liber-v/Avita_Liber_V_Red_Dot_Award_Mob.jpg')"></div>
-                {{-- <div class="banner-bg hidden-sm-down" style="background-image: url('/images/liber-v/AVITA_liber_v_banner_v2_hk_en.jpg')"></div> --}}
-                {{-- <div class="banner-bg hidden-md-up" style="background-image: url('/images/liber-v/AVITA_liber_v_banner_v2_hk_en_mo.jpg')"></div>  --}}
+                
+                
                 <!-- <div class="banner-bg hidden-sm-down" style="background-image: url('/images/liber-v/AVITA_liber_v_amd_banner_en_web.jpg')"></div> -->
                 <!-- <div class="banner-bg hidden-md-up" style="background-image: url('/images/liber-v/AVITA_liber_v_amd_banner_en_mob.jpg')"></div>  -->
             </div>
@@ -309,14 +307,15 @@
 
     </main>
 
-@endsection
+<?php $__env->stopSection(); ?>
 
-@section('css')
-    <link type="text/css" rel="stylesheet" href="{{ asset('css/product-liber.css') }}"/>
-@endsection
+<?php $__env->startSection('css'); ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo e(asset('css/product-liber.css')); ?>"/>
+<?php $__env->stopSection(); ?>
 
-@section('js')
+<?php $__env->startSection('js'); ?>
 
-    <script type="text/javascript" src="{{ asset('js/liber.js') }}"></script>
+    <script type="text/javascript" src="<?php echo e(asset('js/liber.js')); ?>"></script>
 
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
