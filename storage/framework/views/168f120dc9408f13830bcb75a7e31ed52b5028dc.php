564b91b43ac19241b90d7e1b3f63556c2bacfccb
GOD OF THUNDER <avitaind@gmail.com>
2021-05-21 00:47:27 +0530

    cosmos 2 in 1

--- storage/framework/views/168f120dc9408f13830bcb75a7e31ed52b5028dc.php
+++ storage/framework/views/168f120dc9408f13830bcb75a7e31ed52b5028dc.php
@@ -40,11 +40,13 @@
                                        <li class="nav-item">
                                           <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', ['liber-v-gold-blue'])); ?>"><?php echo app('translator')->getFromJson('site.liber-v-gold'); ?></a>
                                        </li>
-                                      
-                                    
                                        <li class="nav-item">
+                                             <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', 'cosmos-2in1')); ?>"><?php echo app('translator')->getFromJson('cosmos.name'); ?></a>
+                                        </li>
+                                    
+                                       <!-- <li class="nav-item">
                                                 <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', 'magus-lite-2in1-laptop')); ?>"><?php echo app('translator')->getFromJson('magus.name'); ?></a>
-                                            </li>
+                                            </li> -->
                                         
                                           <li class="nav-item">
                                                 <a class="nav-link px-md-4 py-2" href="<?php echo e(route('product.overview', 'essential')); ?>"><?php echo app('translator')->getFromJson('site.essential_name'); ?></a>
