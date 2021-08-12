953709a1a2b44fba2ab479a7becff05cd108ab49
GOD OF THUNDER <avitaind@gmail.com>
2021-08-12 11:29:25 +0530

    goa banner

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -27,17 +27,6 @@
         </div>
 
 
-    <!-- <div class="carousel-item">
-            <div class="responsive-block">
-                <a target="_blank" href="https://in.nexstmall.com/">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-lvg"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-lvg"></div>
-                    </div>
-                </a>
-            </div>
-        </div> -->
-
        <div class="carousel-item ">
             <div class="responsive-block">
                     <div class="banner-block responsive-item">
