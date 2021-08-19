aa2683882182dd55f827a660a86478b173e2c094
GOD OF THUNDER <avitaind@gmail.com>
2021-08-19 14:02:32 +0530

    avita live it up banner

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -3,8 +3,21 @@
         <div class="carousel-inner" role="listbox">
 <!---
 --->
-
 <div class="carousel-item active">
+            <div class="responsive-block">
+            <a href="https://promotion.avita.com/?utm_source=minisite&utm_medium=brandsite&utm_campaign=liveitup" target="_blank">
+
+                    <div class="banner-block responsive-item">
+                        <div class="banner-bg hidden-sm-down banner-web-liveitup"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-liveitup"></div>
+                    </div>
+                    </a>
+
+            </div>
+        </div> 
+
+
+<div class="carousel-item">
             <div class="responsive-block">
                     <div class="banner-block responsive-item" controls>
                         
@@ -27,94 +40,13 @@
         </div>
 
 
-       <div class="carousel-item ">
-            <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-99"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-99"></div>
-                    </div>
-            </div>
-        </div> 
-
- <div class="carousel-item ">
-                <div class="responsive-block">
-                <a target="_blank" href="https://www.reliancedigital.in/laptops/c/S101210?searchQuery=AVITA">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-rd"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-rd"></div>
-                    </div>
-               </a>
-          </div>
-     </div>
-
- 
-
-    <div class="carousel-item">
-                <div class="responsive-block">
-               <a href="{{ route('product.overview', ['liber-v']) }}">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-liber-v"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-liber-v"></div>
-                    </div>
-                    </a>
-                </div>
-            </div>
-           
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="{{ route('product.overview', 'cosmos-2in1') }}">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-cosmos2in1"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-cosmos2in1"></div>
-                    </div>
-                    </a>
-                </div>
-        </div>
-
-        <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="{{ route('product.overview', ['essential']) }}">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-essential"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-essential"></div>
-                    </div>
-                    </a>
-                </div>
-        </div>
-
-
-        <div class="carousel-item">
-                <div class="responsive-block">
-                 <a href="{{ route('product.overview', ['admiror']) }}">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-admiror"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-admiror"></div>
-                    </div>
-                </div>
-            </div>
       
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="https://www.avita.com/in/login">
-
-                    <div class="banner-block responsive-item">
-                            <div class="banner-bg hidden-sm-down banner-web-warranty"></div>
-                            <div class="banner-bg hidden-md-up banner-mob-warranty"></div>
-                       </div>
-                       </a>
-                  </div>
-            </div>
-       
+ 
           <ol class="carousel-indicators">
            <li data-target="#home_banner" data-slide-to="0" class="active"></li>
            <li data-target="#home_banner" data-slide-to="1"></li>
            <li data-target="#home_banner" data-slide-to="2"></li>
-           <li data-target="#home_banner" data-slide-to="3"></li>
-           <li data-target="#home_banner" data-slide-to="4"></li>
-           <li data-target="#home_banner" data-slide-to="5"></li>
-           <li data-target="#home_banner" data-slide-to="6"></li>
-           <li data-target="#home_banner" data-slide-to="7"></li>
-
+      
         </ol>
     </div>
   </div>       
--- storage/framework/views/b8d5cac0fe9715f2b6eb2605d77be16b9c49feb7.php
+++ storage/framework/views/b8d5cac0fe9715f2b6eb2605d77be16b9c49feb7.php
@@ -3,119 +3,50 @@
         <div class="carousel-inner" role="listbox">
 <!---
 --->
-
 <div class="carousel-item active">
             <div class="responsive-block">
-                <a target="_blank" href="https://in.nexstmall.com/">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-iamavita"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-iamavita"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
+            <a href="https://promotion.avita.com/?utm_source=minisite&utm_medium=brandsite&utm_campaign=liveitup" target="_blank">
 
-
-    <div class="carousel-item">
-            <div class="responsive-block">
-                <a target="_blank" href="https://in.nexstmall.com/">
                     <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-lvg"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-lvg"></div>
+                        <div class="banner-bg hidden-sm-down banner-web-liveitup"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-liveitup"></div>
                     </div>
-                </a>
-            </div>
-        </div>
+                    </a>
 
-       <div class="carousel-item ">
-            <div class="responsive-block">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-99"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-99"></div>
-                    </div>
             </div>
         </div> 
 
- <div class="carousel-item ">
-                <div class="responsive-block">
-                <a target="_blank" href="https://www.reliancedigital.in/laptops/c/S101210?searchQuery=AVITA">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-rd"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-rd"></div>
-                    </div>
-               </a>
-          </div>
-     </div>
 
- 
-
-    <div class="carousel-item">
-                <div class="responsive-block">
-               <a href="<?php echo e(route('product.overview', ['liber-v'])); ?>">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-liber-v"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-liber-v"></div>
+<div class="carousel-item">
+            <div class="responsive-block">
+                    <div class="banner-block responsive-item" controls>
+                        
+                        <div class="banner-bg hidden-sm-down banner-web-iamavita"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-iamavita"></div>
                     </div>
-                    </a>
-                </div>
+                
             </div>
-           
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="<?php echo e(route('product.overview', 'cosmos-2in1')); ?>">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-cosmos2in1"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-cosmos2in1"></div>
-                    </div>
-                    </a>
-                </div>
         </div>
 
         <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="<?php echo e(route('product.overview', ['essential'])); ?>">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-essential"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-essential"></div>
+            <div class="responsive-block">
+                    <div class="banner-block responsive-item" controls>
+                        
+                        <div class="banner-bg hidden-sm-down banner-web-iamavita-1"></div>                  
+                        <div class="banner-bg hidden-md-up banner-mob-iamavita-1"></div>
                     </div>
-                    </a>
-                </div>
+                
+            </div>
         </div>
 
 
-        <div class="carousel-item">
-                <div class="responsive-block">
-                 <a href="<?php echo e(route('product.overview', ['admiror'])); ?>">
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-admiror"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-admiror"></div>
-                    </div>
-                </div>
-            </div>
       
-            <div class="carousel-item">
-                <div class="responsive-block">
-                <a href="https://www.avita.com/in/login">
-
-                    <div class="banner-block responsive-item">
-                            <div class="banner-bg hidden-sm-down banner-web-warranty"></div>
-                            <div class="banner-bg hidden-md-up banner-mob-warranty"></div>
-                       </div>
-                       </a>
-                  </div>
-            </div>
-       
+ 
           <ol class="carousel-indicators">
            <li data-target="#home_banner" data-slide-to="0" class="active"></li>
            <li data-target="#home_banner" data-slide-to="1"></li>
            <li data-target="#home_banner" data-slide-to="2"></li>
-           <li data-target="#home_banner" data-slide-to="3"></li>
-           <li data-target="#home_banner" data-slide-to="4"></li>
-           <li data-target="#home_banner" data-slide-to="5"></li>
-           <li data-target="#home_banner" data-slide-to="6"></li>
-           <li data-target="#home_banner" data-slide-to="7"></li>
-           <li data-target="#home_banner" data-slide-to="8"></li>
-
+      
         </ol>
     </div>
   </div>       
@@ -127,7 +58,7 @@
 <script type="text/javascript">
         $(document).ready(function() {
     $('.carousel').carousel({
-      interval: 4000
+      interval: 6000
     });
   });
   
