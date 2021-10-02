0fcaa15641e0da6afa925158265304fac3e9905d
avitaind <avitaind@gmail.com>
2021-10-02 15:43:30 +0530

    Banner Update Liber V, Essential, Cosmos, Pura E

--- resources/views/banner_hk.blade.php
+++ resources/views/banner_hk.blade.php
@@ -4,16 +4,65 @@
 <!---
 --->
 <div class="carousel-item active">
-            <div class="responsive-block">
-            <a href="https://promotion.avita.com/?utm_source=minisite&utm_medium=brandsite&utm_campaign=liveitup" target="_blank">
+    <div class="responsive-block">
+    <a href="" target="_blank">
 
-                    <div class="banner-block responsive-item">
-                        <div class="banner-bg hidden-sm-down banner-web-liveitup"></div>                  
-                        <div class="banner-bg hidden-md-up banner-mob-liveitup"></div>
-                    </div>
-                  </a>
+            <div class="banner-block responsive-item">
+                <div class="banner-bg hidden-sm-down banner-web-Liber-V"></div>                  
+                <div class="banner-bg hidden-md-up banner-mob-Liber-V"></div>
             </div>
-        </div> 
+          </a>
+    </div>
+</div> 
+
+<div class="carousel-item ">
+  <div class="responsive-block">
+  <a href="" target="_blank">
+
+          <div class="banner-block responsive-item">
+              <div class="banner-bg hidden-sm-down banner-web-Essential"></div>                  
+              <div class="banner-bg hidden-md-up banner-mob-Essential"></div>
+          </div>
+        </a>
+  </div>
+</div> 
+
+<div class="carousel-item ">
+  <div class="responsive-block">
+  <a href="" target="_blank">
+
+          <div class="banner-block responsive-item">
+              <div class="banner-bg hidden-sm-down banner-web-Cosmos"></div>                  
+              <div class="banner-bg hidden-md-up banner-mob-Cosmos"></div>
+          </div>
+        </a>
+  </div>
+</div> 
+
+<div class="carousel-item ">
+  <div class="responsive-block">
+  <a href="" target="_blank">
+
+          <div class="banner-block responsive-item">
+              <div class="banner-bg hidden-sm-down banner-web-PURA-E"></div>                  
+              <div class="banner-bg hidden-md-up banner-mob-PURA-E"></div>
+          </div>
+        </a>
+  </div>
+</div> 
+
+<div class="carousel-item ">
+  <div class="responsive-block">
+  <a href="https://promotion.avita.com/?utm_source=minisite&utm_medium=brandsite&utm_campaign=liveitup" target="_blank">
+
+          <div class="banner-block responsive-item">
+              <div class="banner-bg hidden-sm-down banner-web-liveitup"></div>                  
+              <div class="banner-bg hidden-md-up banner-mob-liveitup"></div>
+          </div>
+        </a>
+  </div>
+</div> 
+
 <div class="carousel-item">
             <div class="responsive-block">
                     <div class="banner-block responsive-item" controls>
@@ -43,6 +92,10 @@
            <li data-target="#home_banner" data-slide-to="0" class="active"></li>
            <li data-target="#home_banner" data-slide-to="1"></li>
            <li data-target="#home_banner" data-slide-to="2"></li>
+           <li data-target="#home_banner" data-slide-to="3"></li>
+           <li data-target="#home_banner" data-slide-to="4"></li>
+           <li data-target="#home_banner" data-slide-to="5"></li>
+           <li data-target="#home_banner" data-slide-to="6"></li>
       
         </ol>
     </div>
