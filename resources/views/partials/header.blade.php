297ce3ab7ecedf15919a542e96e2e6a1ec5eeb4d
avitaind <52917489+avitaind@users.noreply.github.com>
2022-02-21 13:16:34 +0530

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -220,13 +220,13 @@
                                 <a class="nav-link px-md-4 py-2" target="_blank" href="https://avita-srilanka.com/">Sri Lanka</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link px-md-4 py-2" target="_blank">Bhutan <span class="small">(Launching Soon)</span></a>
+                                <a class="nav-link px-md-4 py-2" target="_blank">Bhutan <span class="small">[Launching Soon]</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link px-md-4 py-2" target="_blank">Afghanistan <span class="small">(Launching Soon)</span></a>
+                                <a class="nav-link px-md-4 py-2" target="_blank">Afghanistan <span class="small">[Launching Soon]</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link px-md-4 py-2" target="_blank">Maldives <span class="small">(Launching Soon)</span></a>
+                                <a class="nav-link px-md-4 py-2" target="_blank">Maldives <span class="small">[Launching Soon]</span></a>
                             </li>
                          </ul>
                     </div>
