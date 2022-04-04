7a1dff2054f4d47693623031a51d265affd24459
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-04-04 18:00:30 +0530

    Ultimus Spec Page V1.002

--- resources/views/product/ultimus/spec_hk_en.blade.php
+++ resources/views/product/ultimus/spec_hk_en.blade.php
@@ -23,11 +23,27 @@
                                 <div class="d-sm-block">AVITA ultimus</div>
                                 <div class="d-sm-block">(35.56 cm)</div>
                             </div>
-                            <img class="hidden-sm-down" src="/images/ultimus/Champaign-Gold.png">
+                            <img class="hidden-sm-down" src="/images/ultimus/champagne_gold.png">
                             <ul
                                 class="list-unstyled spec-color-list d-flex flex-wrap align-items-center justify-content-center mt-4 ">
                                 <li class="active" style="background-color: #bea98d"
-                                    data-image="/images/ultimus/eeeeee.png"></li>
+                                    data-image="/images/ultimus/champagne_gold.png">
+                                </li>
+                                <li class="" style="background-color: #AB3F4C"
+                                    data-image="/images/ultimus/sugar_red.png">
+                                </li>
+                                <li class="" style="background-color: #6D7579"
+                                    data-image="/images/ultimus/space_gray.png">
+                                </li>
+                                <li class="" style="background-color: #73928C"
+                                    data-image="/images/ultimus/shamrock_green.png">
+                                </li>
+                                <li class="" style="background-color: #2B2B2B"
+                                    data-image="/images/ultimus/matt_black.png">
+                                </li>
+                                <li class="" style="background-color: #BBB9BB"
+                                    data-image="/images/ultimus/cloud_silver.png">
+                                </li>
 
                             </ul>
                         </a>
@@ -45,12 +61,12 @@
                 <div class="tab-pane active" id="spec-4" role="tabpanel">
                     <div class="container">
 
-                        <div class="logo-wrapper d-flex px-3 mt-4">
+                        {{-- <div class="logo-wrapper d-flex px-3 mt-4">
                             <div class="offset-md-1">
                                 <img style="width: 200px;" src="{{ asset('images/win10_logo.png') }}"
                                     alt="Windows 10 Home">
                             </div>
-                        </div>
+                        </div> --}}
 
                         <ul class="list-unstyled spec-list">
                             <li class="spec-item d-flex">
@@ -119,7 +135,7 @@
                             </li>
                             <li class="spec-item d-flex">
                                 <div class="offset-md-1 col-4 col-md-3">Battery Life</div>
-                                <div>Up to 6 hours*</div>
+                                <div>Up to 8 hours*</div>
                             </li>
 
                             <li class="spec-item d-flex">
@@ -130,7 +146,7 @@
 
                             <li class="spec-item d-flex">
                                 <div class="offset-md-1 col-4 col-md-3">Accessories</div>
-                                <div>User Guide, Manuals ,AC Adapter and Power cord</div>
+                                <div>User Guide, Manuals, AC Adapter and Power cord</div>
                             </li>
 
                         </ul>
@@ -146,9 +162,10 @@
         <section class="product-statement mt-4 mt-sm-0">
             <div class="container">
                 <ul class="product-statement-list border-top py-2 py-sm-5 mx-auto ls-0 pl-4 py-0">
-                    <li>Windows 10 Home in S mode works exclusively with apps from the Microsoft Store within Windows and
+                    <li>Windows 11 Home works exclusively with apps from the Microsoft Store within Windows and
                         accessories that are compatible with Windows 10 Home in S mode. A one-way switch out of S mode is
-                        available. Learn more at <a href="https://Windows.com/SmodeFAQ"
+                        available. Learn more at <a
+                            href="https://support.microsoft.com/en-gb/windows/windows-10-and-windows-11-in-s-mode-faq-851057d6-1ee9-b9e5-c30b-93baebeebc85#WindowsVersion=Windows_11"
                             target="_blank"><u>Windows.com/SmodeFAQ</u></a>.</li>
                     <li>Centrino Logo, Core Inside, Intel, Intel Logo, Intel Core, Intel Inside, Intel Inside Logo, Intel
                         Viiv, Intel vPro, Itanium, Itanium Inside, Pentium, Pentium Inside, Viiv Inside, vPro Inside, Xeon,
