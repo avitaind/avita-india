a6895d04c3070a1367f44a3ff98f60c6982f919a
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-04-19 18:28:21 +0530

    Ultimus Spec Page V1.62

--- resources/views/product/ultimus/feature_hk_en.blade.php
+++ resources/views/product/ultimus/feature_hk_en.blade.php
@@ -702,7 +702,6 @@
                             <div class="space60 hidden-sm-down"></div>
                             <div class="space60 hidden-sm-down"></div>
                             <div class="space60 hidden-sm-down"></div>
-                            <div class="space60 hidden-sm-down"></div>
 
 
                         </div>
