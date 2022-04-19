83bb4136034ec1eac378057e65bd52a605988629
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-04-19 12:05:52 +0530

    Satus Warranty Tec spec

--- resources/views/product/ultimus/spec_hk_en.blade.php
+++ resources/views/product/ultimus/spec_hk_en.blade.php
@@ -181,6 +181,7 @@
                     <li>1. Weights vary depending on configuration and manufacturing variability.</li>
                     <li>2. Under Test Conditions.</li>
                     <li>For details on ssd upgradation please get it touch with our <a href="{{ route('support') }}" target="_blank"><u>customer care</u></a> via call/whatsapp/email.</li>
+                    <li>NEXSTGO provides a limited warranty period, twelve (12) months for SATUS ULTIMUS for customer who has free carry-in repair service, including parts and labour for the notebook.</li>
 
                 </ul>
             </div>
