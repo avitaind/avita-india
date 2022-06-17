7f192dc98d904258e614d4bad068ae6b54f8479c
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-06-17 11:44:06 +0530

    handleSubscription

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -213,7 +213,7 @@ class HomeController extends Controller
     public function handleSubscription(Request $request, AppMailer $mailer) {
 
         $this->validate($request, [
-             'email'     => 'required|email|unique:users,email',
+             'email'     => ['required', 'string', 'email', 'max:255', 'unique:users'],
             ]);
     
         $subscription = new Subscription([
