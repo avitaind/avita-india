320df9e779d3c0d7f05fde7f08f7b2455b043b3a
Bhavdeep Bharadwaj <63090899+bhavdeepbhardwaj@users.noreply.github.com>
2022-09-07 18:08:34 +0530

    update live v1.6

--- resources/views/product/ultimus/feature_hk_en.blade.php
+++ resources/views/product/ultimus/feature_hk_en.blade.php
@@ -1052,7 +1052,7 @@
                     <li>For details on ssd upgradation please get it touch with our <a href="{{ route('support') }}"
                             target="_blank"><u>customer care</u></a> via call/whatsapp/email.</li>
                     <li>NEXSTGO provides a limited warranty period, twelve (12) months for SATUS ULTIMUS for customer who
-                        has free carry-in repair service, including parts and labour for the notebook.</li>
+                        has "on-site" free repair service, including parts and labour for the notebook.</li>
 
                 </ul>
             </div>
